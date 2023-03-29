8f792e93cd934e69921c143a31ada2f93285e668
Shiv <shiv.7.shan@gmail.com>
2023-03-29 23:01:05 +0530

    chore: add skeleton

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -3,20 +3,24 @@
 import * as React from "react";
 import { cn } from "../helper";
 
-export interface SkeletonProps
-  extends React.HTMLAttributes<HTMLDivElement> {
-    loading: boolean
-  }
+export interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {
+  loading: boolean;
+}
 
-const Skeleton = React.forwardRef<HTMLDivElement, SkeletonProps>(({ className, children, loading, ...props }, ref) => (
-    <div
-    ref={ref}
-    className="relative inline-block"
-    {...props}
-  >
-    {loading && <div className={cn("w-full h-full animate-pulse z-10 bg-neutral-100 dark:bg-neutral-800", className)}/>}
-    {!loading && children}
-  </div>
-));
+const Skeleton = React.forwardRef<HTMLDivElement, SkeletonProps>(
+  ({ className, children, loading, ...props }, ref) => (
+    <div ref={ref} className="relative inline-block" {...props}>
+      {loading && (
+        <div
+          className={cn(
+            "w-full h-full animate-pulse z-10 bg-neutral-100 dark:bg-neutral-800",
+            className
+          )}
+        />
+      )}
+      {!loading && children}
+    </div>
+  )
+);
 
 export { Skeleton };
--- src/stories/Skeleton.stories.tsx
+++ src/stories/Skeleton.stories.tsx
@@ -1,9 +1,7 @@
 import * as React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import { 
-  Skeleton
-} from "../components/Skeleton";
+import { Skeleton } from "../components/Skeleton";
 import { DarkView, LightView } from "./View";
 
 export default {
@@ -13,20 +11,20 @@ export default {
 
 export const Light: ComponentStory<typeof LightView> = () => (
   <LightView className="flex flex-col space-y-2">
-    <Skeleton loading={true} className="h-24 w-24 rounded-full"/>
-    <Skeleton loading={true} className="w-full h-4 rounded-full"/>
-    <Skeleton loading={true} className="w-full h-4 rounded-full"/>
-    <Skeleton loading={true} className="w-full h-4 rounded-full"/>
-    <Skeleton loading={true} className="w-2/3 h-4 rounded-full"/>
+    <Skeleton loading={true} className="h-24 w-24 rounded-full" />
+    <Skeleton loading={true} className="w-full h-4 rounded-full" />
+    <Skeleton loading={true} className="w-full h-4 rounded-full" />
+    <Skeleton loading={true} className="w-full h-4 rounded-full" />
+    <Skeleton loading={true} className="w-2/3 h-4 rounded-full" />
   </LightView>
 );
 
 export const Dark: ComponentStory<typeof DarkView> = () => (
   <DarkView className="flex flex-col space-y-2">
-    <Skeleton loading={true} className="h-24 w-24 rounded-full"/>
-    <Skeleton loading={true} className="w-full h-4 rounded-full"/>
-    <Skeleton loading={true} className="w-full h-4 rounded-full"/>
-    <Skeleton loading={true} className="w-full h-4 rounded-full"/>
-    <Skeleton loading={true} className="w-2/3 h-4 rounded-full"/>
+    <Skeleton loading={true} className="h-24 w-24 rounded-full" />
+    <Skeleton loading={true} className="w-full h-4 rounded-full" />
+    <Skeleton loading={true} className="w-full h-4 rounded-full" />
+    <Skeleton loading={true} className="w-full h-4 rounded-full" />
+    <Skeleton loading={true} className="w-2/3 h-4 rounded-full" />
   </DarkView>
 );
